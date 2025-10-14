f28e3436573b4991a41070acd377ae63804a3a2e
vmarquezperalta13 <vmarquezperalta13@gmail.com>
2025-10-14 17:08:24 +0200

    Creacion Participants ViewModel

--- drinkaholic/lib/main.dart
+++ drinkaholic/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'screens/participants_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
       ),
-      home: const HomeScreen(title: 'Bienvenido a Drinkaholic'),
+      home: const ParticipantsScreen(title: 'Bienvenido a Drinkaholic'),
     );
   }
 }
