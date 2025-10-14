8b7bdbfc008a6008a37c066e88a349ab071c39fa
vmarquezperalta13 <vmarquezperalta13@gmail.com>
2025-10-14 11:13:58 +0200

    Model player

---
+++ drinkaholic/lib/models/player.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+class Player {
+  final int id;
+  final String nombre;
+  final File? imagen; // imagen es nullable
+  final String? avatar; // avatar también es nullable
+
+  Player({
+    required this.id,
+    required this.nombre,
+    this.imagen,
+    this.avatar,
+  });
+}
